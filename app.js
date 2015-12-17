7ba0e77141c58fd6fc8ac911ea5cb483d7876526
mateusz <mateuszalmannai@googlemail.com>
2015-12-17 11:59:31 +0100

    add application structure and model

--- app.js
+++
@@ -1,7 +0,0 @@
-App = Ember.Application.create();
-
-App.Router.map(function() {
-  this.resource("walks", {path: "list-walks"}, function(){
-    this.route("view");
-  });
-});
---
+++ js/app.js
@@ -0,0 +1,13 @@
+App = Ember.Application.create();
+
+App.ApplicationAdapter = DS.LSAdapter.extend({
+  namespace: 'exermatic-data'
+})
+
+App.Walk = DS.Model.extend({
+  dateWalked: DS.attr('date'),
+  distanceWalked: DS.attr('number'),
+  minutesTaken: DS.attr('number'),
+  mood: DS.attr('string')
+})
+
