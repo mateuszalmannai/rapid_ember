bf5e83a29e8961e2db5b2870ecf5d93c0d97cfb4
mateusz <mateuszalmannai@googlemail.com>
2015-12-17 12:28:29 +0100

    fix script tags to use correct corresponding versions

--- js/app.js
+++ js/app.js
@@ -1,13 +1,25 @@
+
 App = Ember.Application.create();
 
 App.ApplicationAdapter = DS.LSAdapter.extend({
   namespace: 'exermatic-data'
-})
+});
 
 App.Walk = DS.Model.extend({
   dateWalked: DS.attr('date'),
   distanceWalked: DS.attr('number'),
   minutesTaken: DS.attr('number'),
   mood: DS.attr('string')
-})
+});
+
+App.Router.map(function () {
+  this.resource('walks');
+  });
+
+App.IndexRoute = Ember.Route.extend({
+  beforeModel: function() {
+    this.transitionTo('walks')
+  }
+});
+
 
